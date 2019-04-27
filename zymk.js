fa022bc56751d0d1c63d5b33021ef5040f7703ee
mieq <pjq940614@live.com>
2019-04-27 13:27:03 +0800

    add

---
+++ zymk.js
@@ -0,0 +1,9 @@
+var obj = JSON.parse(body);
+obj['data']['coins'] = 999;
+obj['data']['vipdate'] = '2020-01-01 00:00:00:00'
+obj['data']['isvip'] = 1
+obj['data']['vipdays'] = 999
+obj['data']['recommend'] = 999
+obj['data']['Cticket'] = 999
+obj['data']['Cgold'] = 999
+JSON.stringify(obj);
