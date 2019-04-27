660d41ad7d789bb07fc2cf4af1e84fec56ed7db8
mieq <pjq940614@live.com>
2019-04-27 13:37:43 +0800

    isvipchange

--- zymk.js
+++ zymk.js
@@ -1,7 +1,7 @@
 var obj = JSON.parse(body);
 obj['data']['coins'] = 999;
 obj['data']['vipdate'] = '2020-01-01 00:00:00:00'
-obj['data']['isvip'] = 1
+obj['data']['isvip'] = 0
 obj['data']['vipdays'] = 999
 obj['data']['recommend'] = 999
 obj['data']['Cticket'] = 999
