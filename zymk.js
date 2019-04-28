2b506d378783513f562f0f88f04db146a23f32a5
mieq <pjq940614@live.com>
2019-04-29 05:11:18 +0800

    a

--- zymk.js
+++ zymk.js
@@ -1,13 +1,11 @@
 let user_info = '/app_api/v5/getuserinfo/';
 let coin_account = '/app_api/v5/coin_account/';
 let ticket = '/app_api/v5/getuserinfo_ticket/';
-let purchase = '/app_api/v5/purchase_chapters_coin/';
+//let purchase = '/app_api/v5/purchase_chapters_coin/';
 var obj = JSON.parse(body);
 
 if (url.indexOf(user_info) != -1) {
 	obj['data']['coins'] = 999;
-	obj['data']['Uviptime'] = 6847833600000;
-	obj['data']['vipdate'] = "2030-01-01 00:00:00";
 	obj['data']['isvip'] = 1;
 	obj['data']['vipdays'] = 999;
 	obj['data']['recommend'] = 999;
@@ -20,13 +18,6 @@ if (url.indexOf(user_info) != -1) {
 } else if (url.indexOf(ticket) != -1) {
 	obj['data']['Cticket'] = 999;
 	obj['data']['recommend'] = 999;	
-} else if (url.indexOf(purchase) != -1) {
-	obj['data']['isvip'] = 1;
-	obj['data']['Cgold'] = 999;	
-	obj['data']['Cticket'] = 999;
-	obj['data']['Cdiamonds'] = 999;
-	obj['status'] = 0;
-	obj['msg'] = "ok";
-}
+} 
 
 JSON.stringify(obj);
