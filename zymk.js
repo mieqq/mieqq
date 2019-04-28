5a6e5fe8c15e9d05a29c65ee9d73a6160a3f4229
mieq <pjq940614@live.com>
2019-04-28 19:35:17 +0800

    update

--- zymk.js
+++ zymk.js
@@ -1,9 +1,21 @@
+let user_info = '/app_api/v5/getuserinfo/';
+let coin_account = '/app_api/v5/coin_account/';
+let ticket = '/app_api/v5/getuserinfo_ticket/';
 var obj = JSON.parse(body);
-obj['data']['coins'] = 999;
-obj['data']['vipdate'] = '2020-01-01 00:00:00:00'
-obj['data']['isvip'] = 1
-obj['data']['vipdays'] = 999
-obj['data']['recommend'] = 999
-obj['data']['Cticket'] = 999
-obj['data']['Cgold'] = 999
+
+if (url.indexOf(user_info) != -1) {
+	obj['data']['coins'] = 999;
+	obj['data']['vipdate'] = '2020-01-01 00:00:00:00'
+	obj['data']['isvip'] = 1;
+	obj['data']['vipdays'] = 999;
+	obj['data']['recommend'] = 999;
+	obj['data']['Cticket'] = 999;
+	obj['data']['Cgold'] = 999;
+} else if (url.indexOf(coin_account) != -1) {
+	obj['data']['coins'] = 999;
+	obj['data']['golds'] = 999;
+} else if (url.indexOf(ticket) != -1) {
+	obj['data']['Cticket'] = 999;
+}
+
 JSON.stringify(obj);
