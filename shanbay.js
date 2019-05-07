6d9b51a21a6f3262e9d63adad1d4e066a284442c
mieq <pjq940614@live.com>
2019-05-07 16:20:58 +0800

    update

--- shanbay.js
+++ shanbay.js
@@ -2,7 +2,7 @@ var obj = JSON.parse(body);
 
 for (var i = 0; i < obj.data.length; i++){
   obj.data[i].due_date = "2030-01-01";
-  obj.data[i].state = "启用";
+  obj.data[i].state_name = "启用";
 }
 
 JSON.stringify(obj);
