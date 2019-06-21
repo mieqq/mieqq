9d60e506599ef07f65e1af1525700af49b4b655d
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-21 17:59:05 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+$httpClient.post({
+		url: "https://dlercloud.me/auth/login",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: {
+			"email": "619478198@qq.com",
+			"passwd": "qq940614",
+			"number-me": "",
+			"code": "",
+			"remember_me": "on"
+		}
+});
+
+$httpClient.post({
+		url: "https://dlercloud.me/user/checkin"
+});
+
+
+$done();
