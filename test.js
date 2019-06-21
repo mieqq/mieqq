766907ef9f7a9e81e93c263164bbd1ff4ba109d9
mieq <pjq940614@live.com>
2019-06-21 19:30:51 +0800

    up

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-//$httpClient.post({
-//		url: "https://dlercloud.me/auth/login",
-//		headers: {
-//			"Content-Type": "application/json"
-//		},
-//		body: {
-//			"email": "619478198@qq.com",
-//			"passwd": "qq940614",
-//			"number-me": "",
-//			"code": "",
-//			"remember_me": "on"
-//		}
-//});
+$httpClient.post({
+		url: "https://dlercloud.me/auth/login",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: {
+			"email": "619478198@qq.com",
+			"passwd": "qq940614",
+			"number-me": "",
+			"code": "",
+			"remember_me": "on"
+		}
+});
 
-const a = $httpClient.post({
+$httpClient.post({
   url: "https://dlercloud.me/user/checkin",
   body: "{}"
 });
-console.log(a)
-$done();
+
+$done()
 
 
