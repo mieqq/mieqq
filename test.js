f148964ae94ddf2a092c2cfdb2d972ea9587a184
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-22 00:33:45 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 function cb1(error, response, data){
-	$httpClient.post("https://dlercloud.me/user/checkin", cb2)
+	$httpClient.post("https://dlercloud.me/user/checkin", cb2);
 }
 
 function cb2(error, response, data){
 	console.log(JSON.parse(data).msg);
-	$done()
 }
 
 
@@ -21,3 +20,5 @@ $httpClient.post({
 			"remember_me": 'on'
 		}
 },cb1)
+
+$done();
