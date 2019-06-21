6914b29b3c4134e355e66a3b789dd99a0e48eef8
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-21 18:04:43 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,19 @@
-$httpClient.post({
-		url: "https://dlercloud.me/auth/login",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: {
-			"email": "619478198@qq.com",
-			"passwd": "qq940614",
-			"number-me": "",
-			"code": "",
-			"remember_me": "on"
-		}
-});
+//$httpClient.post({
+//		url: "https://dlercloud.me/auth/login",
+//		headers: {
+//			"Content-Type": "application/json"
+//		},
+//		body: {
+//			"email": "619478198@qq.com",
+//			"passwd": "qq940614",
+//			"number-me": "",
+//			"code": "",
+//			"remember_me": "on"
+//		}
+//});
 
 $httpClient.post({
 		url: "https://dlercloud.me/user/checkin"
 });
-
 
 $done();
