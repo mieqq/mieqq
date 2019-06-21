2ddd80cac439fc8ec3c9a0f5616a9d47e51594c4
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-21 21:37:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-//async function checkin() {
-//	let resp = await $httpClient.post({
-//		url: URL_PREFIX + "/user/checkin"
-//	});
-//	return resp.data;
-//}
-
 function cb1(value){ console.log(value.error) }
 
 let test = $httpClient.post({
@@ -20,30 +13,5 @@ let test = $httpClient.post({
 			"remember_me": 'on'
 		}
 	}, cb1);
-
-
-
-
-//async function login(email, passwd, code = '') {
-//	await logout()
-//	let resp = await $httpClient.post({
-//		url: URL_PREFIX + "/auth/login",
-//		header: {
-//			"Content-Type": "application/json"
-//		},
-//		body: {
-//			"email": email,
-//			"passwd": passwd,
-//			"number-me": "",
-//			"code": code,
-//			"remember_me": 'on'
-//		}
-//	})
-//	console.log(resp.error)
-//	await checkin()
-//}
-
-
-//let loginRes = login('619478198@qq.com', 'qq940614', '')
 
 $done({})
