43c757aac6e5176b87313c4e014028cd9efbafaa
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-21 22:24:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ function cb1(error, response, data){
 		console.log(error);
 		 }
 
-$httpClient.get("https://www.baidu.com/", cb1(error, response, data));
+$httpClient.get("https://www.baidu.com/", cb1);
 
 $done({})
