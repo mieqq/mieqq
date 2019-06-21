cbe9678395c3654014443bd8ab1cecf1542b0586
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-21 22:01:07 +0800

    Update test.js

--- test.js
+++ test.js
@@ -10,8 +10,9 @@ $httpClient.post({
 			"code": "",
 			"remember_me": 'on'
 		}
-	}, cb1(error, response, data){
-		console.log(error) 
+	}, cb1(error, response, data){ 
+		
+		console.log(error);
 		 });
 
 $done({})
