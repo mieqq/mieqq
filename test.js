ab0a2da6d5da8e07729a32c21db8ac81460b2109
mieq <pjq940614@live.com>
2019-06-21 22:36:16 +0800

    up

--- test.js
+++ test.js
@@ -1,7 +1,25 @@
 function cb1(error, response, data){ 	
-		console.log(response.status);
+		console.log(data);
 		 }
 
 $httpClient.get("https://www.baidu.com/", cb1);
+
+async function login(email, passwd, code = '') {
+	let resp = await $http.post({
+		url: URL_PREFIX + "/auth/login",
+		header: {
+			"Content-Type": "application/json"
+		},
+		body: {
+			"email": email,
+			"passwd": passwd,
+			"number-me": "",
+			"code": code,
+			"remember_me": 'on'
+		}
+}, cb1)
+
+login("619478198@qq.com", "qq940614", "")
+	
 
 $done({})
