28306c0743c3925b0ef58a1a457c9761a312c1b3
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-22 12:07:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,26 +1,16 @@
-function cb1(error, response, data){
-    console.log(error)
-	$httpClient.post("https://dlercloud.me/user/checkin", cb2);
-	$done();
-}
-
-function cb2(error, response, data){
-	console.log("1")
-}
-
-
 $httpClient.post({
-		url: "https://dlercloud.me/auth/login",
+		url: "https://baidu.com",
 		header: {
 			"Content-Type": "application/json"
 		},
 		body: {
-			"email": "619478198@qq.com",
-			"passwd": "qq940614",
+			"email": "",
+			"passwd": "",
 			"number-me": "",
 			"code": "",
 			"remember_me": 'on'
 		}
-},cb1)
+},function(error, response, data){
+	console.log(error)
 
-$done();
+});
