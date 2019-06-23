e536e96b3c7b76464fc3288a0a284b9053854f18
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-23 16:20:40 +0800

    Update dlerCheckin.js

--- dlerCheckin.js
+++ dlerCheckin.js
@@ -4,25 +4,44 @@ const table = {
 		"Content-Type": "application/json"
 	},
 	body: {
-		"email": "",
-		"passwd": "",
+		"email": "619478198@qq.com",
+		"passwd": "qq940614",
 		"number-me": "",
 		"code": "",
 		"remember_me": 'on'
 	}
 }
 
+function getNetworkStatus(data) {
+	let res = { usage: 0, detail: '' }
+	let usageReg = /width: (.*?)%;/
+	let detailRege = /(可用：.*?(?:K|M|G|T)B)[\s\S]*?(已用：.*?(?:K|M|G|T)B)/
+	if (usageReg.test(data)) {
+		res.usage = RegExp.$1
+	}
+	if (detailRege.test(data)) {
+		res.detail = `${RegExp.$1} / ${RegExp.$2}`
+	}
+	return res
+}
+
 $httpClient.post(table, function(error, response, data){
 	if (error){
 		console.log(error);
-//		$notification.post('Checkin', error, "");
+		$notification.post('Dler Cloud', error, "");
 		$done();				   
 	} else {
 		$httpClient.post("https://dlercloud.io/user/checkin", function(error, response, data){
 			console.log(JSON.parse(data).msg);
-//			$notification.post('Checkin', JSON.parse(data).msg, "");
-			$done();
+			let checkin = JSON.parse(data).msg
+			$httpClient.get("https://dlercloud.io/user", function(error, response, data){
+				let userinfo = getNetworkStatus(data)
+				$notification.post('Dler Cloud', checkin, userinfo.detail);
+				$done();
+			});
+
 		});
+		
 	}	
 });
 
@@ -34,4 +53,4 @@ $httpClient.post(table, function(error, response, data){
 //[Script]
 //cron "0 * * * *" script-path=test.js
 //4. 上面的 crontab "0 * * * *" 为每小时0分触发，可以自己查表更改
-//5. 需要通知可以取消 $notification 行的注释
+//5. 不需要通知可以注释掉 $notification 所在行
