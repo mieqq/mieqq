17db803df4cf946cb078ab5a5b83115e16ae3357
mieqq <49203595+mieqq@users.noreply.github.com>
2019-06-30 00:23:26 +0800

    Update zymk.js

--- zymk.js
+++ zymk.js
@@ -2,25 +2,24 @@ let user_info = '/app_api/v5/getuserinfo/';
 let coin_account = '/app_api/v5/coin_account/';
 let ticket = '/app_api/v5/getuserinfo_ticket/';
 // let purchase = '/app_api/v5/purchase_chapters_coin/';
-var obj = JSON.parse(body);
+let body = JSON.parse($response.body);
 
 if (url.indexOf(user_info) != -1) {
-	obj['data']['coins'] = 999;
-	obj['data']['isvip'] = 1;
-	obj['data']['vipdays'] = 999;
-	obj['data']['recommend'] = 999;
-	obj['data']['Cticket'] = 999;
-	obj['data']['Cgold'] = 999;
+	body.data.coins = 999;
+	body.data.isvip = 1;
+	body.data.recommend = 999;
+	body.data.Cticket = 999;
+	body.data.Cgold = 999;
 } else if (url.indexOf(coin_account) != -1) {
-	obj['data']['coins'] = 999;
-	obj['data']['golds'] = 999;
+	body.data.coins = 999;
+	body.data.golds = 999;
 } else if (url.indexOf(ticket) != -1) {
-	obj['data']['Cticket'] = 999;
-	obj['data']['recommend'] = 999;	
+	body.data.Cticket = 999;
+	body.data.recommend = 999;	
 } 
 
-JSON.stringify(obj);
+$done({body});
 
-// http-response getuserinfo-globalapi.zymk.cn script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/zymk.js
+// http-response getuserinfo-globalapi.zymk.cn script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/zymk.js,requires-body = true
 
 // hostname = getuserinfo-globalapi.zymk.cn
