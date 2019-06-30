6721583e4809e5dfac2c0710f72dd4156b527279
mieq <pjq940614@live.com>
2019-06-30 12:09:21 +0800

    i

--- zymk.js
+++ zymk.js
@@ -2,23 +2,19 @@ let user = '/app_api/v5/getuserinfo/';
 let coin = '/app_api/v5/coin_account/';
 let ticket = '/app_api/v5/getuserinfo_ticket/';
 
-let body = "";
+let body = JSON.parse(response.body);
 
 if ($request.url.indexOf(user) != -1) {
-    body = JSON.parse($response.body);
 	body.data.coins = 999;
 	body.data.isvip = 1;
 	body.data.recommend = 999;
 	body.data.Cticket = 999;
 	body.data.Cgold = 999;
-} else if ($request.url.indexOf(coin) != -1){   body = JSON.parse($response.body);
+} else if ($request.url.indexOf(coin) != -1){   
 	body.data.coins = 999;
 	body.data.golds = 999;
 } else if ($request.url.indexOf(ticket) != -1) {
-    body = JSON.parse($response.body);
 	body.data.Cticket = 999;	
-} else {
-    $done({});
 }
 
 body = JSON.stringify(body);
