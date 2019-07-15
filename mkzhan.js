356898a2cd1ea4396db65a23d34dbf2ae14ca937
mieq <pjq940614@live.com>
2019-07-15 20:14:31 +0800

    u

--- mkzhan.js
+++ mkzhan.js
@@ -2,24 +2,7 @@ let buy = "comic.mkzcdn.com/chapter/buy/add/"
 let info = "member.mkzcdn.com/user/funds/info/"
 
 let url = $request.url;
-let obj = "";
+let body = "";
 
-if (url.indexOf(info) != -1) {
-	obj = JSON.parse($response.body);
-	obj.data.gold = "999";
-	obj.data.ticket = "999";
-	obj.data.is_vip = "1";
-	obj.data.integral = "999";
-	obj.data.card_count = "999";
-	obj.data.vip_end_time = "1599999999";
-	obj.data.read_card_end_time = "1599999999";
-} else if (url.indexOf(buy) != -1) {
-	obj = JSON.parse($response.body);
-	obj.code = "200";
-	obj.message = "购买成功";
-} else {
-	$done({});
-}
+$done({});
 
-obj = JSON.stringify(obj);
-$done({body: obj});
