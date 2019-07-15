1d701523dd5907416fa2625187823cbafa3d8512
mieq <pjq940614@live.com>
2019-07-15 20:11:19 +0800

    u

--- mkzhan.js
+++ mkzhan.js
@@ -2,24 +2,24 @@ let buy = "comic.mkzcdn.com/chapter/buy/add/"
 let info = "member.mkzcdn.com/user/funds/info/"
 
 let url = $request.url;
-let body = ""
+let obj = "";
 
 if (url.indexOf(info) != -1) {
-	body = JSON.parse($response.body);
-	body.data.gold = "999";
-	body.data.ticket = "999";
-	body.data.is_vip = "1";
-	body.data.integral = "999";
-	body.data.card_count = "999";
-	body.data.vip_end_time = "1599999999";
-	body.data.read_card_end_time = "1599999999";
+	obj = JSON.parse($response.body);
+	obj.data.gold = "999";
+	obj.data.ticket = "999";
+	obj.data.is_vip = "1";
+	obj.data.integral = "999";
+	obj.data.card_count = "999";
+	obj.data.vip_end_time = "1599999999";
+	obj.data.read_card_end_time = "1599999999";
 } else if (url.indexOf(buy) != -1) {
-	body = JSON.parse($response.body);
-	body.code = "200";
-	body.message = "购买成功";
+	obj = JSON.parse($response.body);
+	obj.code = "200";
+	obj.message = "购买成功";
 } else {
 	$done({});
 }
 
-body = JSON.stringify(body);
-$done({body});
+obj = JSON.stringify(obj);
+$done({body: obj});
