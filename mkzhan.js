8a16bf602bb31aed889c818a64c4e1bfd5e162ef
mieq <pjq940614@live.com>
2019-07-15 19:17:04 +0800

    u

--- mkzhan.js
+++ mkzhan.js
@@ -6,6 +6,7 @@ body.data.is_vip = "1";
 body.data.integral = "999";
 body.data.card_count = "999";
 body.data.vip_end_time = "1599999999";
+body.data.read_card_end_time = "1599999999";
 
 body = JSON.stringify(body);
 
