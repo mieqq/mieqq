b60c05aa12e6526a36679f3dce6e745870203ff9
mieqq <49203595+mieqq@users.noreply.github.com>
2020-11-07 13:44:07 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -5,7 +5,8 @@ CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
 yum install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v2.0.1/snell-server-v2.0.1-linux-amd64.zip
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v2.0.3/snell-server-v2.0.3-linux-amd64.zip
+
 unzip -o snell.zip
 rm -f snell.zip
 chmod +x snell-server
