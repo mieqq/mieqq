63359d743355e802e321fdc7c6eed6cf5f0d118a
mieqq <49203595+mieqq@users.noreply.github.com>
2021-02-28 00:50:58 +0800

    Update xiaoxiao.js

--- xiaoxiao.js
+++ xiaoxiao.js
@@ -1,22 +1,9 @@
-let request = {
-    url: $request.url,
-    headers: $request.headers,
-};
-
-$httpClient.get(request, function(error, response, data) {
-    let body = JSON.parse(data); 
-    let headers = response.headers;
-    delete headers["Transfer-Encoding"]
-    callback(body, headers)
-})
-
-
 const path1 = "/ucp/index";
 const ad = 'ssp-svr/ssp/list3';
 const adold = 'getGlobalData';
+const url = $request.url;
 
-function callback(body, headers) {
-    let url = $request.url;
+function buildReponse(body, headers) {
     if (url.indexOf(path1) != -1){
 	body.data.uinfo["down_daily_remainders"] = "666";
 	body.data.uinfo["play_daily_remainders"] = "666";
@@ -24,15 +11,29 @@ function callback(body, headers) {
 	body.data.user["isvip"] = "1";
 	body.data.user["goldcoin"] = "666";
 }
-
     if (url.indexOf(ad) != -1) {
          delete body.data.pmap
     }
-
     if (url.indexOf(adold) != -1) {
          delete body.data.adrows
          delete body.data.adgroups
          delete body.data.iOS_adgroups
     }
-    $done({response: {body: JSON.stringify(body), headers}});
+    return {response: {body: JSON.stringify(body), headers}};
 }
+
+let request = {
+    url: url,
+    headers: $request.headers,
+};
+
+$httpClient.get(request, function(error, response, data) {
+    if (error) {
+        $done();
+    } else {
+        let body = JSON.parse(data); 
+        let headers = response.headers;
+        delete headers["Transfer-Encoding"]
+        $done(buildReponse(body, headers));
+    }
+});
