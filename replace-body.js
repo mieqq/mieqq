5d515af3cb2ad4264368b3b7a273d482f7570923
mieq <pjq940614@live.com>
2022-04-14 15:57:47 +0800

    update

--- replace-body.js
+++ replace-body.js
@@ -7,7 +7,7 @@
    可改写为 Surge：
    test = type=http-response,pattern=https://service.ilovepdf.com/v1/user,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/replace-body.js,argument=false=true
    
-  argument=匹配值=替换值，可以用正则匹配，如argument=/\w+/=test，也支持多参数，如：argument=匹配值1=替换值1&匹配值2=替换值2
+  argument=匹配值=替换值，可以用正则匹配，如argument=/\w+/g=test，也支持多参数，如：argument=匹配值1=替换值1&匹配值2=替换值2
   
   response-body 脚本类型选择 type=http-response、request-body 脚本类型选择 type=http-request，注意必须打开需要body（requires-body=1）
 */
