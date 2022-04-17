4068f7fc1ecde7917ae2255fe038fac605603a8d
mieq <pjq940614@live.com>
2022-04-17 11:05:05 +0800

    u

--- replace-body.js
+++ replace-body.js
@@ -2,18 +2,14 @@
   Surge 脚本实现 Qx 的 response-body、和request-body 重写类型
   
   如 Qx：
-https://service.ilovepdf.com/v1/user url response-body false response-body true
+  https://service.ilovepdf.com/v1/user url response-body false response-body true
    
-可改写为 Surge：
-[Script]   
-test = type=http-response,pattern=https://service.ilovepdf.com/v1/user,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/replace-body.js,argument=false=true
+   可改写为 Surge：
+   test = type=http-response,pattern=https://service.ilovepdf.com/v1/user,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/replace-body.js,argument=false=true
    
-  argument=匹配值=替换值
-  支持正则：如argument=\w+=test
-  支持正则修饰符：如argument=/\w+/g=test
-  支持多参数，如：argument=匹配值1=替换值1&匹配值2=替换值2
+  argument=匹配值=替换值，可以用正则匹配，如argument=/\w+/g=test，也支持多参数，如：argument=匹配值1=替换值1&匹配值2=替换值2
   
-  支持改写响应体和请求体体（type=http-response、request-body 注意必须打开需要body（requires-body=1）
+  response-body 脚本类型选择 type=http-response、request-body 脚本类型选择 type=http-request，注意必须打开需要body（requires-body=1）
   
   tips 修改json格式的键值对可以这样：
   argument=("key"):\s?"(.+?)"=$1: "new_value"
@@ -42,7 +38,7 @@ if (typeof $argument == "undefined") {
 	}
 
 	$argument.split("&").forEach((item) => {
-		let [match, replace] = item.split("=");
+		let [match, replace] = item.split(/=(?=[^=]+$)/);
 		let re = getRegexp(match);
 		body = body.replace(re, replace);
 	});
