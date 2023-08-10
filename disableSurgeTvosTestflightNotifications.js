3d1c6d35e45e38509bb4b018158ed7427c71e440
mieqq <49203595+mieqq@users.noreply.github.com>
2023-08-10 10:58:15 +0800

    Delete disableSurgeTvosTestflightNotifications.js

--- disableSurgeTvosTestflightNotifications.js
+++
@@ -1,8 +0,0 @@
-if (!$request.body) $done({});
-let body = JSON.parse($request.body);
-body.platformUpdates.appletvos.emailEnabled = false;
-body.platformUpdates.appletvos.pushEnabled = false;
-$notification.post("","已禁用通知","请删除本模块");
-$done({body: JSON.stringify(body)});
-
-
