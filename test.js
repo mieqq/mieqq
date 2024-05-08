7822274a6facb3c5f3be75a9335e58082cc06e93
mieqq <49203595+mieqq@users.noreply.github.com>
2024-05-08 22:52:39 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 [Script]
-test = type=cron,cronexp=* * * * *,script-path=https://github.com/mieqq/mieqq/blob/master/test2.js?raw=true,script-update-interval=0,engine=auto
+test = type=cron,cronexp=* * * * *,script-path=https://github.com/mieqq/mieqq/blob/master/test2.js?raw=true,script-update-interval=0,engine=jsc
