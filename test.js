05d0824cc983288467de02e95f3d97c863158554
mieqq <49203595+mieqq@users.noreply.github.com>
2024-05-08 22:49:06 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-$notification.post("hello3","","")
-$done()
+test = type=cron,cronexp=* * * * *,script-path=https://github.com/mieqq/mieqq/blob/master/test2.js?raw=true,script-update-interval=0,engine=auto
